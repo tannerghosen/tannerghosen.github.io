041985cc01731fd1780a712f66d722bf4c8b944c
Tanner Ghosen <tannerghosen@yahoo.com>
2023-11-21 21:40:25 -0500

    Removed old drop down header animation for a new one that types itself out (sort of).

--- src/main.js
+++ src/main.js
@@ -6,83 +6,68 @@ if (!localStorage.getItem("mode"))
 var project = 1;
 var maxprojects = 4;
 var slideshow = false;
-
 // Functions
-document.addEventListener("DOMContentLoaded", () =>
-{
+document.addEventListener("DOMContentLoaded", () => {
 
-// Light/Dark Mode Function
-	function LightSwitch()
-	{
-		 // toggles lightmode from all tags in document.body (everything)
-		document.body.classList.toggle("lightmode"); 
+	// Light/Dark Mode Function
+	function LightSwitch() {
+		// toggles lightmode from all tags in document.body (everything)
+		document.body.classList.toggle("lightmode");
 	}
 
 	if (localStorage.getItem("mode") === "light") // if the last time we were on the page it was light mode (not default), turn on light mode.
 	{
 		LightSwitch();
 		document.body.classList.add("notransition"); //  without this, you'll see the dark mode briefly as it'll do a transition effect to switch to light mode on the code being ran.
-		setTimeout(function()
-		{
+		setTimeout(function () {
 			document.body.classList.remove("notransition"); // we don't want it forever because that'll mess up future transitions.
-		},500);
+		}, 500);
 	}
-	
-	document.getElementById("lightswitch").addEventListener("click", () =>
-	{
-		if (localStorage.getItem("mode") == "dark") 
-		{ 
+
+	document.getElementById("lightswitch").addEventListener("click", () => {
+		if (localStorage.getItem("mode") == "dark") {
 			localStorage.setItem("mode", "light"); // we set it to light mode if it was dark mode
 			LightSwitch();
 		}
-		else if (localStorage.getItem("mode") == "light") 
-		{
+		else if (localStorage.getItem("mode") == "light") {
 			localStorage.setItem("mode", "dark"); // vice versa of above
 			LightSwitch();
 		}
 	});
 
-// Time Function
-	function TheTime() 
-	{ 
+	// Time Function
+	function TheTime() {
 		// time.getMonth()+1 if you use x/x/xxxx for day format
 		let time = new Date();
 		let months = ["Janurary", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 		let [month, day, year, hour, minute, second, period] = [months[time.getMonth()], time.getDate(), time.getFullYear(), time.getHours(), time.getMinutes(), time.getSeconds(), "AM"];
 		// it will only show the minute/second without a 0 in front of it (should it be less than 10) without these two ifs
-		if (minute < 10)
-		{
-			minute = "0"+minute;
+		if (minute < 10) {
+			minute = "0" + minute;
 		}
 
-		if (second < 10)
-		{
-			second = "0"+second;
+		if (second < 10) {
+			second = "0" + second;
 		}
 
-		if (hour >= 12)
-		{
+		if (hour >= 12) {
 			period = "PM";
 		}
-		else if (hour <= 11)
-		{
+		else if (hour <= 11) {
 			period = "AM";
 		}
 
-		if (hour > 12) 
-		{
+		if (hour > 12) {
 			// time uses the 24 hour clock, we want a normal clock, so remove 12 hours if it's greater than that.
 			hour -= 12;
 		}
 
-		if (hour == 0)
-		{
+		if (hour == 0) {
 			// we don't want midnight showing up as 0.
 			hour = 12;
 		}
 
-		if(document.getElementById("time"))
-		{
+		if (document.getElementById("time")) {
 			document.getElementById("time").innerHTML = `Today is ${month} ${day}, ${year} and the time is ${hour}:${minute}:${second} ${period}.`;
 		}
 	}
@@ -90,51 +75,67 @@ document.addEventListener("DOMContentLoaded", () =>
 	TheTime();
 	let timer = setInterval(() => TheTime(), 1000);
 
-// Slideshow
+	// Slideshow
 	// Back Button
-	document.getElementById("backbutton").addEventListener("click", () =>
-	{
-		if(project == 1) // if we are at the first project, go to the last project.
+	document.getElementById("backbutton").addEventListener("click", () => {
+		if (project == 1) // if we are at the first project, go to the last project.
 		{
 			project = maxprojects;
-			document.getElementById("project1").setAttribute("hidden","");
-			document.getElementById("project"+project).removeAttribute("hidden");
+			document.getElementById("project1").setAttribute("hidden", "");
+			document.getElementById("project" + project).removeAttribute("hidden");
 		}
-		else
-		{
+		else {
 			project--;
-			document.getElementById("project"+(project+1)).setAttribute("hidden","");
-			document.getElementById("project"+project).removeAttribute("hidden");
+			document.getElementById("project" + (project + 1)).setAttribute("hidden", "");
+			document.getElementById("project" + project).removeAttribute("hidden");
 		}
 	});
 	// Next Button
-	document.getElementById("nextbutton").addEventListener("click", () =>
-	{
-		if(project == maxprojects) // if we are at the last project, go to the first project.
+	document.getElementById("nextbutton").addEventListener("click", () => {
+		if (project == maxprojects) // if we are at the last project, go to the first project.
 		{
 			project = 1;
-			document.getElementById("project"+maxprojects).setAttribute("hidden","");
-			document.getElementById("project"+project).removeAttribute("hidden");
+			document.getElementById("project" + maxprojects).setAttribute("hidden", "");
+			document.getElementById("project" + project).removeAttribute("hidden");
 		}
-		else
-		{
+		else {
 			project++;
-			document.getElementById("project"+(project-1)).setAttribute("hidden","");
-			document.getElementById("project"+project).removeAttribute("hidden");
+			document.getElementById("project" + (project - 1)).setAttribute("hidden", "");
+			document.getElementById("project" + project).removeAttribute("hidden");
 		}
 	});
 
-// Links
-	document.body.addEventListener("click", (e) =>
-	{
-		if (e.target.tagName == "A")
-		{
-			if (e.target.href.includes("http") == false)
-			{
+	// Links
+	document.body.addEventListener("click", (e) => {
+		if (e.target.tagName == "A") {
+			if (e.target.href.includes("http") == false) {
 				e.preventDefault(); // this is we so we don't reload the page on clicking a non http/s link.
 			}
 		}
 	});
+
+	// Typewriter
+	let header = document.getElementById("header");
+	let i = 0; // iterator
+	let text = ""; // text to output
+	let speed = 200; // speed of the typewriter in ms
+	let thepage = localStorage.getItem("lastpage"); // we grab the lastpage so if the page changes we stop the typewriter
+	function TypeWriter()
+	{
+		if (i < text.length && thepage == localStorage.getItem("lastpage")) // if i < text.length and thepage matches the current page
+		{
+			header.innerHTML += text.charAt(i); // add the letter at i
+			i++; // increase iterator
+			setTimeout(TypeWriter, speed); // recursively call the function after the speed ms
+		}
+	}
+	// this is so we don't get <empty string> errors from trying to get innerHTML too early
+	setTimeout(() => {
+		text = header.innerHTML; // set text to header's innerHTML
+		header.style.visibility = "visible"; // make header visible
+		header.innerHTML = ""; // clear its innerHTML
+		TypeWriter(); // call typewriter
+	}, 500);
 
 // Router (part 1)
 	const app = document.getElementById("app");
